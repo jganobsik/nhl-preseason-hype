433af3dd517c5fdca218b65cc7f2657bf8f770f8
Joe Ganobsik <jganobsik@gmail.com>
2019-07-28 22:31:37 -0400

    import teams containers

---
+++ src/Containers/menu.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import NavBar from '../Components/NavBar'
+
+export default class Menu extends Component {
+    render() {
+        return (
+            <div>
+                <NavBar />
+            </div>
+        )
+    }
+}
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,41 @@ import ReactDOM from 'react-dom';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
+
+import Menu from '../src/Containers/menu'
+// Import Team Containers
+import Canes from '../src/Containers/Metro/Canes'
+import Jackets from '../src/Containers/Metro/Jackets'
+import Devils from '../src/Containers/Metro/Devils'
+import Islanders from '../src/Containers/Metro/Islanders'
+import Rangers from '../src/Containers/Metro/Rangers'
+import Flyers from '../src/Containers/Metro/Flyers'
+import Pens from '../src/Containers/Metro/Pens'
+import Caps from '../src/Containers/Metro/Caps'
+import Bruins from '../src/Containers/Atlantic/Bruins'
+import Sabres from '../src/Containers/Atlantic/Sabres'
+import Wings from '../src/Containers/Atlantic/Wings'
+import Panthers from '../src/Containers/Atlantic/Panthers'
+import Habs from '../src/Containers/Atlantic/Habs'
+import Leafs from '../src/Containers/Atlantic/Leafs'
+import Lightning from '../src/Containers/Atlantic/Lightning'
+import Sens from '../src/Containers/Atlantic/Sens'
+import Hawks from '../src/Containers/Central/Hawks'
+import Avs from '../src/Containers/Central/Avs'
+import Blues from '../src/Containers/Central/Blues'
+import Stars from '../src/Containers/Central/Stars'
+import Wild from '../src/Containers/Central/Wild'
+import Preds from '../src/Containers/Central/Preds'
+import Jets from '../src/Containers/Central/Jets'
+import Yotes from '../src/Containers/Pacific/Yotes'
+import Flames from '../src/Containers/Pacific/Flames'
+import Oilers from '../src/Containers/Pacific/Oilers'
+import Kings from '../src/Containers/Pacific/Kings'
+import Sharks from '../src/Containers/Pacific/Sharks'
+import Canucks from '../src/Containers/Pacific/Canucks'
+import Knights from '../src/Containers/Pacific/Knights'
+import Ducks from '../src/Containers/Pacific/Ducks'
+
 import * as serviceWorker from './serviceWorker';
 const routes = (
 <Router>
